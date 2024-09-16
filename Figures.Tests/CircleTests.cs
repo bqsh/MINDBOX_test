dabb91edba3aae2e9832bb75012ae074a8694e9a
bituum <adegtev@unistory.app>
2024-09-16 22:36:00 +0300

    Make circle test prettier

--- Figures.Tests/CircleTests.cs
+++ Figures.Tests/CircleTests.cs
@@ -9,7 +9,7 @@ public class CircleTests
     [Test]
     public void Constructor_ValidRadius_ShouldCreateCircle()
     {
-        double radius = 5.0;
+        var radius = 5.0;
 
         var circle = new Circle(radius);
 
@@ -19,20 +19,20 @@ public class CircleTests
     [Test]
     public void Constructor_NegativeOrZeroRadius_ShouldThrowArgumentOutOfRangeException()
     {
-        double radius = -1.0;
+        var radius = -1.0;
 
-        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(radius));
-        Assert.That(ex.Message, Does.Contain("radius cannot be lesser than 0!"));
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => _ = new Circle(radius));
+        Assert.That(ex?.Message, Does.Contain("radius cannot be lesser than 0!"));
     }
 
     [Test]
     public void CalculateArea_ValidCircle_ShouldReturnCorrectArea()
     {
-        double radius = 3.0;
+        var radius = 3.0;
         var circle = new Circle(radius);
-        double expectedArea = Math.PI * Math.Pow(radius, 2);
+        var expectedArea = Math.PI * Math.Pow(radius, 2);
 
-        double area = circle.CalculateArea();
+        var area = circle.CalculateArea();
 
         Assert.That(expectedArea, Is.EqualTo(area).Within(0.00001));
     }
